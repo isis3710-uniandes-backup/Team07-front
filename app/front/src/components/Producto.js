94de485344777872a1eadc9720a4d54d3e1cf71b
santiago rodriguez matiz <s.rodriguezm@uniandes.edu.co>
2019-03-28 11:25:10 -0500

    coso

---
+++ app/front/src/components/Producto.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import shoes from '../ui/mockups/shoes.jpg'
+const Producto = props =>{
+  return(
+    <div clasName="card text-center">
+      <divc className="overflow">
+        <img src={shoes} alt='Image 1'/>
+      </div>
+      <div clasName="card-body text-dark">
+      <h4 clasName="card-title">Card Title</h4
+      <p className="cart-text text-secondary">
+      Lorem ipsum u puta madre
+      </p>
+      <a href>
+    </div>
+  );
+}
+export default Producto;
--- app/front/src/index.js
+++ app/front/src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css
+import Producto from 'components/Producto';
+
+ReactDOM.render(<Producto />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
