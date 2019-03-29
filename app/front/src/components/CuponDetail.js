aaa6b1e1ec85ed3437923a027cb76fd198b308a1
dan26 <jd.carrillor@uniandes.edu.co>
2019-03-29 08:35:10 -0500

    fin

--- app/front/src/components/CuponDetail.js
+++ app/front/src/components/CuponDetail.js
@@ -4,9 +4,14 @@ import './css/producto-style.css';
 import {Link} from 'react-router-dom';
 import axios from 'axios'
 class CuponDetail extends Component{
+
+
+
+
   constructor(props){
     super(props);
     this.state={
+      id:0,
       pagina: 0,
       idMarca:0,
       idTienda:0,
@@ -27,6 +32,7 @@ class CuponDetail extends Component{
                     state.idMarca = cupon.idMarca;
                     //state.productos = tienda.productos;
                     state.idTienda = cupon.idTienda;
+                    state.id=id
                     //tiendas
                     //promociones
                     
@@ -55,7 +61,42 @@ class CuponDetail extends Component{
         
       }
       axios.post('http://localhost:3001/cupones', cupon);
+      alert('Creacion correcta')
+    }
+
+      putCupon=()=>{
+      let idMarca=document.getElementById('idMarcaPut').value;
+      if(idMarca==0){
+        idMarca=this.state.idMarca;
+      }
+      let idTienda=document.getElementById('idTiendaPut').value;
+      if(idTienda==0){
+        idTienda=this.state.idTienda;
+      }
+      let valor=document.getElementById('valorPut').value;
+      if(valor==0){
+        valor=this.state.valor;
+      }
+
+       let fechaVencimiento=document.getElementById('fechaVencimientoPut').value;
+      if(fechaVencimiento==""){
+        fechaVencimiento=this.state.fechaVencimiento;
+      }
       
+      let cupon={
+        idMarca:idMarca,
+        idTienda:idTienda,
+        valor:valor,
+        fechaVencimiento:fechaVencimiento
+        
+      }
+      axios.put('http://localhost:3001/cupones/'+this.state.id, cupon);
+      console.log(this.state.id);
+      console.log("aaaaaaaaa");
+    }
+    deleteCupon=()=>{
+      axios.delete('http://localhost:3001/cupones/'+this.state.id);
+      window.location.href = 'http://localhost:3000/CuponList';
     }
   render(){
     return(
@@ -75,6 +116,8 @@ class CuponDetail extends Component{
             <Link to={{
               pathname:"/CuponList",
             }}  className="btn btn-outline-primary">Volver</Link>
+            <button type="button" className="btn btn-danger float-right" onClick={this.deleteCupon}>Eliminar</button>
+
             </div>
         </div>
       </div>
@@ -117,44 +160,34 @@ class CuponDetail extends Component{
         <h1>Actualizar </h1>
         <form class="form-horizontal" action="/action_page.php">
           <div class="form-group">
-            <label class="control-label col-sm-12" for="email">Nombre producto:</label>
+            <label class="control-label col-sm-12" >ID de la marca del cupon:</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" id="email" placeholder={this.state.nombre}/>
+              <input type="text" class="form-control" id="idMarcaPut" placeholder="ID"/>
             </div>
           </div>
+        
         <div class="form-group">
-          <label class="control-label col-sm-12" for="pwd">Url de imagen:</label>
+          <label class="control-label col-sm-12" >ID de la tienda del cupon::</label>
         <div class="col-sm-10">
-          <input type="url" class="form-control" id="pwd" placeholder={this.state.src}/>
-        </div>
-        </div>
-        <div class="form-group">
-          <label class="control-label col-sm-12" for="pwd">Clasificación:</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="pwd" placeholder={this.state.clasificacion}/>
+          <input type="text" class="form-control" id="idTiendaPut" placeholder="ID"/>
         </div>
         </div>
         <div class="form-group">
-          <label class="control-label col-sm-12" for="pwd">Talla disponible:</label>
+          <label class="control-label col-sm-12" >Valor del producto:</label>
         <div class="col-sm-10">
-          <input type="text" class="form-control" id="pwd" placeholder={this.state.talla}/>
+          <input type="text" class="form-control" id="valorPut" placeholder={this.state.talla}/>
         </div>
         </div>
         <div class="form-group">
-          <label class="control-label col-sm-12" for="pwd">Precio de venta:</label>
+          <label class="control-label col-sm-12" >Fecha de vencimiento del producto:</label>
         <div class="col-sm-10">
-          <input type="text" class="form-control" id="pwd" placeholder={this.state.precio}/>
+          <input type="text" class="form-control" id="fechaVencimientoPut" placeholder="Formato DD-MM-AAAA"/>
         </div>
         </div>
-        <div class="form-group">
-          <label class="control-label col-sm-12" for="pwd">Cantidad disponible:</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="pwd" placeholder={this.state.cant}/>
-        </div>
-        </div>
+        
         <div class="form-group">
           <div class="col-sm-offset-2 col-sm-10">
-            <button class="btn btn-info" onClick={this.postProducto}>Crear</button>
+            <button class="btn btn-info" onClick={this.putCupon}>Actualizar</button>
           </div>
         </div>
         </form >
--- app/front/src/components/TiendaDetail.js
+++ app/front/src/components/TiendaDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import shoes from '../ui/mockups/shoes.jpg'
 import './css/producto-style.css';
 import {Link} from 'react-router-dom';
 import axios from 'axios'
@@ -7,42 +7,88 @@ class TiendaDetail extends Component{
   constructor(props){
     super(props);
     this.state={
+      id:0,
       pagina: 0,
       nombre:"",
       productos:[],
       direccion:"",
-      tiendas:[],
+      marcas:[],
       promociones:[],
       imagen:""
-     
+      
     };
   }
 
   componentDidMount() {
-        const id=this.props.match.params.idTienda;
-        console.log(id);
+        var id=this.props.match.params.idTienda;
         var i;
             axios.get('http://localhost:3001/tiendas/'+id)
                 .then((response) => {
                     var state = this.state;
-                    var tienda = response.data;
+                    var tienda= response.data;
+                    state.id = id;
                     state.nombre = tienda.nombre;
-                    //state.productos = tienda.productos;
                     state.direccion = tienda.direccion;
-                    //tiendas
-                    //promociones
-                    
-                    
                     state.imagen = tienda.imagen;
                     
+                
                     state.pagina = 0;
                     // pueden cambiar el tamaño de partion aca
                     this.setState(state);
                 });
     }
+    postTienda=()=>{
+      let nombre=document.getElementById('nombrePost').value;
+      let direccion=document.getElementById('direccionPost').value;
+    
+      let img=document.getElementById('urlPost').value;
+     
+  
+      let tienda={
+        nombre:nombre,
+        direccion:direccion,
+        imagen:img
+        
+        
+                
+      }
+      axios.post('http://localhost:3001/tiendas', tienda);
+      alert('Creacion correcta');
+    
+    }
+    putTienda=()=>{
+      let nombre=document.getElementById('nombrePut').value;
+      if(nombre==""){
+        nombre=this.state.nombre;
+      }
+      let direccion=document.getElementById('direccionPut').value;
+      if(direccion==""){
+        direccion=this.state.direccion;
+      }
+    
+      let img=document.getElementById('urlPut').value;
+      if(img==""){
+        img=this.state.imagen;
+      }
+  
+     
+      let tienda={
+        nombre:nombre,
+        imagen:img,
+        direccion:direccion,
+      
+      }
+      axios.put('http://localhost:3001/tiendas/'+this.state.id, tienda);
+      console.log(this.state.id);
+    }
+    deleteTienda=()=>{
+      axios.delete('http://localhost:3001/tiendas/'+this.state.id);
+      window.location.href = 'http://localhost:3000/TiendaList';
+      console.log(this.state.id);
+    }
   render(){
     return(
-      <div>
+      <div className="container">
       <div className="float-left">
         <div className="card text-center eso">
           <div className="overflow">
@@ -51,43 +97,76 @@ class TiendaDetail extends Component{
             <div className="card-body text-dark">
             <h4 className="card-title">{this.state.nombre}</h4>
             <p className="card-text text-secondary">Direccion: {this.state.direccion}</p>
-            <p className="card-text text-secondary">Clasificacion: {this.state.clasificacion}</p>
-            <p className="card-text text-secondary">Productos: Camiseta polo</p>
-            <p className="card-text text-secondary">Productos: Jean 911 Levi's</p>
-            
-            <p className="card-text text-secondary">Marcas:Mario Hernandez </p>
-            <p className="card-text text-secondary">Promociones :Madrugon Falabella </p>
-            <p className="card-text text-secondary">Promociones :BlackFriday </p>
-            <Link to={{   
-              pathname:"/TiendaList",
-            }}  className="btn btn-outline-primary">Volver</Link>
+            <p className="card-text text-secondary">Productos: Camiseta Polo</p>
+            <p className="card-text text-secondary">Productos: Tenis Reebok</p>
+            <p className="card-text text-secondary">Promociones: Madrugon Falabella</p>
+            <p className="card-text text-secondary">Promociones: BlackFriday</p>
+            <p className="card-text text-secondary">Descripcion: {this.state.descripcion}</p>
+            <div className="container">
+            <Link to={{
+              pathname:"/MarcaList",
+            }}  className="btn btn-outline-primary float-left">Volver</Link>
+            <button type="button" className="btn btn-danger float-right" onClick={this.deleteTienda}>Eliminar</button>
+            </div>
             </div>
         </div>
       </div>
       <div className="float-right">
-        <form class="form-horizontal" action="/action_page.php">
-          <div class="form-group">
-            <label class="control-label col-sm-2" for="email">Email:</label>
-            <div class="col-sm-10">
-              <input type="email" class="form-control" id="email" placeholder="Enter email"/>
+        <h1>Crear un nuevo </h1>
+        <form className="form-horizontal">
+          <div className="form-group">
+            <label className="control-label col-sm-12">Nombre producto:</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="nombrePost" placeholder="Enter name"/>
             </div>
           </div>
-        <div class="form-group">
-          <label class="control-label col-sm-2" for="pwd">Password:</label>
-        <div class="col-sm-10">
-          <input type="password" class="form-control" id="pwd" placeholder="Enter password"/>
+        <div className="form-group">
+          <label className="control-label col-sm-12">Url de imagen:</label>
+        <div className="col-sm-10">
+          <input type="url" className="form-control" id="urlPost" placeholder="Enter url"/>
         </div>
+        </div>
+        <div className="form-group">
+          <label className="control-label col-sm-12">Direccion:</label>
+        <div className="col-sm-10">
+          <input type="text" className="form-control" id="direccionPost" placeholder="Enter tags"/>
+        </div>
+        </div>
+        
+      
+        <div className="form-group">
+          <div className="col-sm-offset-2 col-sm-10">
+              <button className="btn btn-success" onClick={this.postTienda}>Crear</button>
+          </div>
+        </div>
+        </form >
       </div>
-        <div class="form-group">
-          <div class="col-sm-offset-2 col-sm-10">
-            <div class="checkbox">
-              <label><input type="checkbox"/> Remember me</label>
+      <div className="float-left abc">
+        <h1>Actualizar </h1>
+        <form className="form-horizontal">
+          <div className="form-group">
+            <label className="control-label col-sm-12">Nombre producto:</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="nombrePut" placeholder={this.state.nombre}/>
             </div>
           </div>
+        <div className="form-group">
+          <label className="control-label col-sm-12" >Url de imagen:</label>
+        <div className="col-sm-10">
+          <input type="url" className="form-control" id="urlPut" placeholder={this.state.src}/>
         </div>
-        <div class="form-group">
-          <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-default">Submit</button>
+        </div>
+        <div className="form-group">
+          <label className="control-label col-sm-12" >Direccion:</label>
+        <div className="col-sm-10">
+          <input type="text" className="form-control" id="direccionPut" placeholder={this.state.clasificacion}/>
+        </div>
+        </div>
+        
+       
+        <div className="form-group">
+          <div className="col-sm-offset-2 col-sm-10">
+            <button className="btn btn-info" onClick={this.putTienda}>Actualizar</button>
           </div>
         </div>
         </form >
